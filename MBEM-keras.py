7637330961f396a0f7b557d5423ebede07035895
Asish Chakrapani <35690826+aplusc98@users.noreply.github.com>
2019-07-25 15:30:12 +0530

    Update MBEM-keras.py

--- MBEM-keras.py
+++ MBEM-keras.py
@@ -23,34 +23,11 @@ conf = generate_workers(m,k,gamma,class_wise)
 print ("\nnumber of training examples: " + str(samples) + "\t redundancy: " + str(repeat))
 # calling the main function
 resp_org, workers_train_label_org, workers_val_label, workers_this_example, var, temp= generate_labels_weight(train_y,test_y,n,n1,repeat,conf)    
-########geting the vote count for classes #########################################################
-e = []
-g = np.zeros((n,k))
-for i in range(n):
-    v =Counter(temp[i])
-    e = list(v.keys())
-    for w in range(len(e)):
-            g[i][w] = e[w]
-    if len(e) < 10:
-        kp = np.arange(w+1,10)
-        g[i][kp] = -1
-###############################################converting into custom representation##
-z = np.zeros((n,k))
-for i in range(n):
-    for j in range(k):
-        if g[i][j] != -1:
-            a  = copy.deepcopy(g[i][j])
-            a = int(a)
-            z[i][a] = 1
-########################################################################################
-y_dummy = np.zeros((n,k))
-for i in range(50000):
-    y_dummy[i,0] = i
 ########################################################################################
 print ("Algorithm: majority vote:\t\t"),
 pred_mv = majority_voting(resp_org)   
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-t_score, tes_score, _ = train(x_train, pred_mv, y_dummy,z, x_test, y_test)
+t_score, tes_score, _ = train(x_train, pred_mv, x_test, y_test)
 print ("train error:  " + str(t_score))
 print ("Test error:  " + str(tes_score))
 ##########################################################################################
@@ -60,7 +37,7 @@ for r in range(repeat):
     naive_agg = naive_agg + (1/repeat)*copy.deepcopy(workers_train_label_org['softmax'+ str(r) +'_label']) 
 # calling the "call_train" function which besides printing the generalization error 
 # returns model prediction on the training examples, which is being stored in the variable "naive_pred".
-t_score, tes_score, labels = train(x_train, naive_agg, y_dummy,z, x_test, y_test)
+t_score, tes_score, labels = train(x_train, naive_agg, x_test, y_test)
 print ("train error:  " + str(t_score))
 print ("Test error:  " + str(tes_score))
 #########################################################################################
@@ -70,7 +47,7 @@ algo_agg = np.zeros((n,k))
 algo_agg = probs_est_labels
 # calling the "call_train" function with aggregated labels being the posterior probability distribution of the 
 # examples given the model prediction obtained using the "weighted majority vote" algorithm.
-t_score, tes_score, _ = train(x_train, algo_agg, y_dummy,z, x_test, y_test)
+t_score, tes_score, _ = train(x_train, algo_agg, x_test, y_test)
 print ("train error:  " + str(t_score))
 print ("Test error:  " + str(tes_score))
 ##########################################################################################
